bbc3a966b96bd06c934c3ec2944aee9354e00430
Baronzinho <gabrielbaron2014@gmail.com>
2020-09-02 22:40:15 -0300

    Inclusão de arquivos

---
+++ Java/Inter/src/Model/DML.java
@@ -0,0 +1,97 @@
+package concessionaria;
+
+import java.sql.SQLException;
+
+public class DML extends SQL {
+
+    public DML(String tabela) {
+        super(tabela);
+    }
+
+    public void insertCustomer(int customerNumber, String customerName, String contactFirstName, String contactLastName,
+            String phone, String addressLine, String city, String country) throws SQLException {
+        sql = "insert into " + tabela
+                + " (customerNumber, "
+                + "customerName, "
+                + "contactFirstName, "
+                + "contactLastName, "
+                + "phone, "
+                + "addressLine1, "
+                + "city, "
+                + "country) "
+                + "values("
+                + customerNumber + ", '"
+                + customerName + "', '"
+                + contactFirstName + "', '"
+                + contactLastName + "', '"
+                + phone + "', '"
+                + addressLine + "', '"
+                + city + "', '"
+                + country + "');";
+        update(sql);
+    }
+
+    public void updateCustomer(int customerNumber, String customerName, String contactFirstName, String contactLastName, String phone, String addressLine1, String city, String country) throws SQLException {
+        sql = "update " + super.tabela
+                + " set customerName = '" + customerName
+                + "', contactLastName = '" + contactLastName
+                + "', contactFirstName = '" + contactFirstName
+                + "', phone = '" + phone
+                + "', addressLine1 = '" + addressLine1
+                + "', city = '" + city
+                + "', country = '" + country
+                + "'where customerNumber = " + customerNumber + ";";
+        update(sql);
+    }
+
+    public void deleteCustomer(int customerNumber) throws SQLException {
+        sql = "delete from " + tabela
+                + " where customerNumber = " + customerNumber + ";";
+        update(sql);
+    }
+
+    public void insertProduct(String productCode, String productName, String productLine, String productScale, String productVendor, String productDescription, int quantityInStock, double buyPrice, double MSRP) throws SQLException {
+        sql = "insert into " + super.tabela
+                + " (productCode, "
+                + "productName, "
+                + "productLine, "
+                + "productScale, "
+                + "productVendor, "
+                + "productDescription, "
+                + "quantityInStock, "
+                + "buyPrice, "
+                + "MSRP) "
+                + "values('"
+                + productCode + "', '"
+                + productName + "', '"
+                + productLine + "', '"
+                + productScale + "', '"
+                + productVendor + "', '"
+                + productDescription + "', "
+                + quantityInStock + ", "
+                + buyPrice + ", "
+                + MSRP + ");";
+        update(sql);
+    }
+
+    public void updateProduct(String productCode, String productName, String productLine, String productScale, String productVendor, String productDescription, int quantityInStock, double buyPrice, double MSRP) throws SQLException {
+        sql = "update " + super.tabela
+                + " set productCode = '" + productCode
+                + "', productName = '" + productName
+                + "', productLine = '" + productLine
+                + "', productScale = '" + productScale
+                + "', productVendor = '" + productVendor
+                + "', productDescription = '" + productDescription
+                + "', quantityInStock = " + quantityInStock
+                + ", buyPrice = " + buyPrice
+                + ", MSRP = " + MSRP
+                + " where productCode = '" + productCode + "';";
+        update(sql);
+    }
+
+    public void deleteProduct(String productCode) throws SQLException {
+        sql = "delete from " + super.tabela
+                + " where productCode = '" + productCode + "';";
+        update(sql);
+    }
+}
---
+++ Java/Inter/src/Model/SQL.java
@@ -0,0 +1,70 @@
+package concessionaria;
+
+import conexao.BDFabricaConexao;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+public abstract class SQL {
+
+    protected ResultSet rset;
+    protected String tabela, sql;
+
+    public SQL(String tabela) {
+        this.tabela = tabela;
+    }
+
+    protected void select(String sql) throws SQLException {
+        try {
+            Connection con = (Connection) BDFabricaConexao.getConnection();
+            Statement stm = (Statement) con.createStatement();
+            rset = stm.executeQuery(sql);
+            while (rset.next()) {
+                JOptionPane.showMessageDialog(null, "SELECT: " + rset.getString("name_manager"));
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(SQL.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    protected void update(String sql) throws SQLException {
+        try {
+            Connection con = (Connection) BDFabricaConexao.getConnection();
+            Statement stm = (Statement) con.createStatement();
+            stm.executeUpdate(sql);
+        } catch (SQLException ex) {
+            Logger.getLogger(SQL.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    protected void conect(String sql) throws SQLException {
+        try {
+            Connection con = (Connection) BDFabricaConexao.getConnection();
+            Statement stm = (Statement) con.createStatement();
+            rset = stm.executeQuery(sql);
+        } catch (SQLException ex) {
+            Logger.getLogger(SQL.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    ResultSet getRset() {
+        return rset;
+    }
+
+    String getTabela() {
+        return tabela;
+    }
+
+    void setRset(ResultSet rset) {
+        this.rset = rset;
+    }
+
+    void setTabela(String tabela) {
+        this.tabela = tabela;
+    }
+}
