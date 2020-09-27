14ccb4c359673385363e42ee998268caf7fd48fc
CaioSMota <caiomota802@gmail.com>
2020-09-27 02:23:00 -0300

    Quase Finalizado

--- Java/Inter/src/DAO/AulaMarcadaDAO.java
+++ Java/Inter/src/DAO/AulaMarcadaDAO.java
@@ -22,7 +22,7 @@ public class AulaMarcadaDAO extends SQL{
     public List <AulaMarcada> exibeAulasMarcadasAluno(int id_Aluno) {
         String status = "Confirmado";
         List<AulaMarcada> listaAulas = new ArrayList();
-        sql = "SELECT * FROM AulasMarcadasAluno WHERE id_Aluno = " + id_Aluno + " AND statusDaAula = '" + status +"' ;";
+        sql = "SELECT * FROM AulasMarcadasAluno WHERE id_Aluno = " + id_Aluno + " AND statusDaAula = '" + status +"' AND data_Marcada >= (SELECT CURRENT_TIMESTAMP) ;";
         try {
             rset = select(sql);
             while (rset.next()) {
@@ -70,7 +70,31 @@ public class AulaMarcadaDAO extends SQL{
     public List <AulaMarcada> exibeAulasMarcadasProfessor(int id_Prof) {
         String status = "Confirmado";
         List<AulaMarcada> listaAulas = new ArrayList();
-        sql = "SELECT * FROM AulasMarcadasProfessor WHERE id_Aluno = " + id_Prof + " AND statusDaAula = '" + status +"';";
+        sql = "SELECT * FROM AulasMarcadasProfessor WHERE id_Professor = " + id_Prof + " AND statusDaAula = '" + status +"' AND data_Marcada >= (SELECT CURRENT_TIMESTAMP);";
+        try {
+            rset = select(sql);
+            while (rset.next()) {
+                AulaMarcada aula = new AulaMarcada();
+                aula.setId_Aluno(rset.getInt(1));
+                aula.setId_Professor(rset.getInt(2));
+                aula.setId_Aula_Marcada(rset.getInt(3));
+                aula.setNome(rset.getString(4));
+                aula.setMateria_Professor(rset.getString(5));
+                aula.setData_Aula(rset.getDate(6));
+                aula.setHora_Aula(rset.getString(7));
+                listaAulas.add(aula);
+            }
+        }catch (SQLException ex) {
+            System.out.println("ERRO: Lista Não foi Retornada ");
+            return null;
+        }    
+        return listaAulas;
+    }
+    
+    public List <AulaMarcada> exibeAulasMarcadasProfessorPesquisa(int id_Prof,String pesquisa) {
+        String status = "Confirmado";
+        List<AulaMarcada> listaAulas = new ArrayList();
+        sql = "SELECT * FROM AulasMarcadasProfessor WHERE id_Professor = " + id_Prof + " AND statusDaAula = '" + status +"' AND nome LIKE '%"+pesquisa+"%' AND data_Marcada >= (SELECT CURRENT_TIMESTAMP);";
         try {
             rset = select(sql);
             while (rset.next()) {
@@ -95,7 +119,7 @@ public class AulaMarcadaDAO extends SQL{
         String status = "Pendente";
         List<AulaMarcada> listaAulas = new ArrayList();
         sql = "SELECT * FROM AulasPendentesProfessor "
-                + "WHERE id_Professor = " + id_Prof + " AND statusDaAula = '" + status +"';";
+                + "WHERE id_Professor = " + id_Prof + " AND statusDaAula = '" + status +"' AND data_Marcada >= (SELECT CURRENT_TIMESTAMP);";
 
         try {
             rset = select(sql);
@@ -117,10 +141,25 @@ public class AulaMarcadaDAO extends SQL{
         return listaAulas;
     }
     
+    public ResultSet retornaAulaMarcadaProfessor(int id) throws SQLException{
+         sql = "SELECT * FROM AulasMarcadasProfessor WHERE id_Aula_Marcada = " + id + ";";
+         rset = select(sql);
+        return rset;
+     }
     
     public ResultSet retornaAulaMarcada(int id) throws SQLException{
          sql = "SELECT data_Marcada, hora_Aula FROM Aula_Marcada where id_Professor = " + id + ";";
          rset = select(sql);
         return rset;
      }
+    
+    public void recusaAula(int id)throws SQLException{        
+        sql = "UPDATE Aula_Marcada SET statusDaAula = '" + "Recusada" + "'WHERE id_Aula_Marcada = '" + id + "';";
+        update(sql);
+    }
+    
+    public void confirmaAula(int id)throws SQLException{        
+        sql = "UPDATE Aula_Marcada SET statusDaAula = '" + "Confirmado" + "'WHERE id_Aula_Marcada = '" + id + "';";
+        update(sql);
+    }
 }
